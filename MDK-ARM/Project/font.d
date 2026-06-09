8acc7f7da27b8add448fca0a97b8b8ee73abb6e2
BASH <766679965@qq.com>
2026-06-09 12:20:42 +0800

    添加 .gitignore，移除编译产物

--- MDK-ARM/Project/font.d
+++
@@ -1,4 +0,0 @@
-project\font.o: ..\Core\Src\font.c
-project\font.o: ../Core/Inc/font.h
-project\font.o: C:\Keil_MDK\core\ARM\ARMCC\Bin\..\include\stdint.h
-project\font.o: C:\Keil_MDK\core\ARM\ARMCC\Bin\..\include\string.h
